d97488b007aab4f3c3344685bdd68455d5b49b99
hanumansaini12511 <er.hanumansaini@gmail.com>
2023-04-17 19:08:39 +0530

    dd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import Home from './view/screen/Home'
 import Details from './view/screen/Details'
-import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+import {BrowserRouter as Router,Route,Routes, Link} from 'react-router-dom'
 import Addtocart from './view/screen/Addtocat';
 import './view/style/style.css'
+import {menubar} from './view/data/data'
 function App() {
   return (
 <>
 <Router>
+  <div className='menubar'>
+    <ul>
+<Link to="/" className='Link'><li>Home</li></Link>
+  {
+    menubar.map(d=>(
+      <Link to={`/${d}`} className='Link'><li>{d}</li></Link>
+    ))
+  }
+  </ul>
+  </div>
 <Routes>
   <Route path="/" element={<Home />} />
   <Route path="/details" element={<Details />} />
