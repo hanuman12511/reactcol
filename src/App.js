8ad1794e4dc09ef3454157fb7468fc06770418b3
hanumansaini12511 <er.hanumansaini@gmail.com>
2023-04-18 11:33:14 +0530

    ser

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {useState} from 'react'
 import Home from './view/screen/Home'
 import Details from './view/screen/Details'
 import {BrowserRouter as Router,Route,Routes, Link} from 'react-router-dom'
@@ -5,19 +6,43 @@ import Addtocart from './view/screen/Addtocat';
 import './view/style/style.css'
 import {menubar} from './view/data/data'
 function App() {
+  const[isVisible,setAccount] = useState(false);
+
+  let isAccount=()=>{
+    //alert("account")
+    setAccount(!isVisible)
+  }
+let isRegister=()=>{
+    alert("register")
+    setAccount(!isVisible)
+  }
+  let isLogin=()=>{
+    alert("login")
+    setAccount(!isVisible)
+  }
   return (
 <>
 <Router>
-  <div className='menubar'>
-    <ul>
-<Link to="/" className='Link'><li>Home</li></Link>
-  {
-    menubar.map(d=>(
+<div className='menubar'>
+  <div className='menu-div'>
+      <ul>
+    <Link to="/" className='Link'><li>Home</li></Link>
+    {
+      menubar.map(d=>(
       <Link to={`/${d}`} className='Link'><li>{d}</li></Link>
     ))
-  }
+    }
   </ul>
   </div>
+  <div className='logo-div'>
+    <img src={require('./view/img/icon/account.png')}  className='accountlogo' onClick={isAccount} />
+   { isVisible&& <div className='accountdrop'>
+       <li onClick={isRegister}>Register</li>
+        <li onClick={isLogin}>Login</li>
+    </div>
+    }
+  </div>
+</div>
 <Routes>
   <Route path="/" element={<Home />} />
   <Route path="/details" element={<Details />} />
